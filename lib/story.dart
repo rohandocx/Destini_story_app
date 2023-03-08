39828548bfe381a8808fa535a1d1c30c9c5aac2e
Your Name <rohan2056.be20@chitkara.edu.in>
2023-03-08 20:34:25 +0530

    First  commit

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story {
+  String storytitle;
+  String choice1;
+  String choice2;
+
+  Story({String storyTitle, String choice1, String choice2}) {
+    this.storytitle = storyTitle;
+    this.choice1 = choice1;
+    this.choice2 = choice2;
+  }
+}
